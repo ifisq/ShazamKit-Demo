186a5d4694db200cbf37d70ef0ead0897987350f
Aryan Nambiar <aryanjnambiar@gmail.com>
2023-01-25 14:58:26 -0600

    Initial Commit

---
+++ Shazam/ShazamApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShazamApp.swift
+//  Shazam
+//
+//  Created by Aryan Nambiar on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ShazamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
